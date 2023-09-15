777f8013c01f6be7ebec18389682ca1cd4a8491b
Talha Malik <talha.malik@A006-01262.local>
2023-09-15 18:05:38 +0500

    Eslint integrated in react app

---
+++ library-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Mui Installed</Button>
+    </div>
+  );
+}
+
+export default App;
