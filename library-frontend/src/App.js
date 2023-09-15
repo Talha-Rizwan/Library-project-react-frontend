eec8003e34bb6f2717797380c6f6110287f79e79
Talha Malik <talha.malik@A006-01262.local>
2023-09-15 18:18:21 +0500

    Feat: added signup/signin templete pages

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 
+import './App.css';
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 function App() {
   return (
     <div>
-      <Button variant="contained">Mui Installed</Button>
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Button variant="contained">Mui Installed</Button>} />
+        <Route path="/signup/" element={<SignUp/>} />
+        <Route path="/login/" element={<SignIn/>} />
+        <Route path="/*" element={<h1>error 404 the page is not found</h1>} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
