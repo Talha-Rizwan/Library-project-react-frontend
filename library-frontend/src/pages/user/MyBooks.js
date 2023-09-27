0ba079142382a6513a69967e92a3bb831451597f
Talha Malik <talha.malik@A006-01262.local>
2023-09-27 16:02:41 +0500

    removed unnecessary imports

--- library-frontend/src/pages/user/MyBooks.js
+++ library-frontend/src/pages/user/MyBooks.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 
 import { REQUEST_STATUS, URL } from "../../constants";
